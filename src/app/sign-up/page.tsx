03cfa178e3f3a0c4e759dabdd32098d93c89602e
Yassine Abdelouafi <abdelouafi.yassine@gmail.com>
2026-02-11 17:37:23 +0100

    110226-netlify

--- src/app/sign-up/page.tsx
+++
@@ -1,7 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default function SignUpPage() {
-  // This will redirect anyone who tries to visit /sign-up back to the home page
-  redirect("/");
-  return null;
-}   
